322b7dbfa64b5ca7da7d4c44b658f6864d7587cb
sgrass <xgrass@foxmail.com>
2018-01-06 16:49:45 +0800

    init commit

--- src/main/java/org/cx/lock/ReentrantLockDemo.java
+++ src/main/java/org/cx/lock/ReentrantLockDemo.java
@@ -4,9 +4,31 @@ import java.util.concurrent.locks.ReentrantLock;
 
 
 /**
- * 
- * 重入锁不会阻塞(同步块会阻塞)
- * 可以反复进入
+ *
+ * ReentrantLock是基于AQS实现的
+ * 在AQS内部会保存一个状态变量state，通过CAS修改该变量的值，修改成功的线程表示获取到该锁，
+ * 没有修改成功，或者发现状态state已经是加锁状态，则通过一个Waiter对象封装线程，添加到等待队列中，并挂起等待被唤醒
+ * {@link java.util.concurrent.locks.AbstractQueuedSynchronizer#acquire}
+ *
+ * CAS参数,第一个参数是要修改的对象，第二个参数是对象中要修改变量的偏移量，第三个参数是修改之前的值，第四个参数是预想修改后的值
+ *
+ * CAS存在ABA问题,{@AtomicStampedReference} 或者 {@link java.util.concurrent.locks.StampedLock} 解决
+ *
+ * 公平锁:
+ *  static final class FairSync extends Sync 每个线程的资源竞争是公平的.按照自身线程调用lock方法的顺序来获取锁,即先到先得.
+ * 非公平锁:
+ *  static final class NonfairSync extends Sync 每个线程的资源竞争是顺序不定,谁的优先级高,那么谁就会先获得锁.
+ * 无参构造方法默认将局部变量sync赋值为NonfairSycn
+ *
+ * acquire(1)一般参数为0是释放锁,参数为1是获取锁
+ *
+ *
+ * 使用reentrantLock的场景
+ *  1.可以指定是公平锁还是非公平锁。而synchronized只能是非公平锁。所谓的公平锁就是先等待的线程先获得锁。
+ *  2.提供了一个Condition（条件）类，用来实现分组唤醒需要唤醒的线程们，而不是像synchronized要么随机唤醒一个线程要么唤醒全部线程。
+ *  3.提供了一种能够中断等待锁的线程的机制，通过lock.lockInterruptibly()来实现这个机制。
+ *
+ *
  * 支持锁中断防止死锁，优先响应中断
  * 支持限时获取锁
  * @author grass
--- src/main/java/org/cx/lock/StampLockDemo.java
+++ src/main/java/org/cx/lock/StampLockDemo.java
@@ -5,13 +5,25 @@ import java.util.concurrent.locks.StampedLock;
 /**
  * jdk8 javadoc demo
  *
+ * 类似 {@link java.util.concurrent.atomic.AtomicStampedReference} 通过一个版本号，解决ABA问题
+ *
+ *
+ *
  * @author grass
  * @date 2017/11/21
  */
 public class StampLockDemo {
+    /**
+     * 坐标x，y，一个修改，一个读。
+     */
     private double x, y;
     private final StampedLock sl = new StampedLock();
 
+    /**
+     * 独占锁，写锁，排他性，阻塞读。
+     * @param deltaX
+     * @param deltaY
+     */
     void move(double deltaX, double deltaY) { // an exclusively locked method 完全锁定的方法
         long stamp = sl.writeLock();
         try {
@@ -22,7 +34,10 @@ public class StampLockDemo {
         }
     }
 
-    //乐观锁
+    /**
+     * 乐观读，对读共享，对写不排他，允许写。写少读多
+     * @return
+     */
     double distanceFromOrigin() { // A read-only method
         long stamp = sl.tryOptimisticRead();  //获得一个乐观读锁
         double currentX = x, currentY = y;  //将两个字段读入本地局部变量
@@ -35,15 +50,21 @@ public class StampLockDemo {
                 sl.unlockRead(stamp);
             }
         }
+        //最后执行返回，可以是乐观读后的数据，或者是普通读后的数据。
         return Math.sqrt(currentX * currentX + currentY * currentY);
     }
 
-    //悲观锁
+
+    /**
+     * 普通读锁，对读共享，对写排他， 读少写多
+     * @param newX
+     * @param newY
+     */
     void moveIfAtOrigin(double newX, double newY) { // upgrade
         // Could instead start with optimistic, not read mode
         long stamp = sl.readLock();
         try {
-            while (x == 0.0 && y == 0.0) { //循环，检查当前状态是否符合
+            while (x == 0.0 && y == 0.0) { //循环，检查当前状态是否符合.没有改成功，就一直阻塞。
                 long ws = sl.tryConvertToWriteLock(stamp); //将读锁转为写锁
                 if (ws != 0L) { //确认转为写锁是否成功
                     stamp = ws; //如果成功 替换票据
